72cec355974a5c3d62cd484f85d20577ea15b5cc
Ben Smith <ben.smith.dev0@gmail.com>
2023-05-17 23:26:37 -0500

    Change to be a stateless component.

--- src/features/userSearch/UserCard.tsx
+++ src/features/userSearch/UserCard.tsx
@@ -1,46 +1,49 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '../../app/store';
+import { PublicGitHubUser } from '../../common/services/publicGitHubApi';
 import styles from './userCard.module.css';
 
-const UserCard: React.FC = () => {
-  const user = useSelector((state: RootState) => state.user);
+export interface UserCardProps {
+  user: PublicGitHubUser | null;
+}
 
+const UserCard: React.FC<UserCardProps> = ({ user }: UserCardProps) => {
   const joinedDate = new Date(user?.created_at ?? '');
   const joinedYear = joinedDate.getFullYear();
 
   return (
-    <a
-      href={user?.html_url}
-      target="_blank"
-      rel="noreferrer"
-      className={`
+    <div>
+      <a
+        href={user?.html_url}
+        target="_blank"
+        rel="noreferrer"
+        className={`
       ${styles.userCard}
       ${styles.limitedStyle}
       `}
-    >
-      <div className={styles.cardHeader}>
-        <img
-          src={user?.avatar_url}
-          alt="GitHub user's avatar."
-          className={styles.userAvatar}
-        ></img>
-        <div>
-          <h1>{user?.name}</h1>
-          <h2>{user?.login}</h2>
+      >
+        <div className={styles.cardHeader}>
+          <img
+            src={user?.avatar_url}
+            alt="GitHub user's avatar."
+            className={styles.userAvatar}
+          ></img>
+          <div>
+            <h1>{user?.name}</h1>
+            <h2>{user?.login}</h2>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.cardContent}>
-        <p>{user?.bio ?? 'No bio available.'}</p>
-      </div>
+        <div className={styles.cardContent}>
+          <p>{user?.bio ?? 'No bio available.'}</p>
+        </div>
 
-      <div className={styles.cardFooter}>
-        <p>Followers: {user?.followers ?? 0}</p>
-        <p>Following: {user?.following ?? 0}</p>
-        <p>Public Repos: {user?.public_repos ?? 0}</p>
-        <p>Joined {joinedYear}</p>
-      </div>
-    </a>
+        <div className={styles.cardFooter}>
+          <p>Followers: {user?.followers ?? 0}</p>
+          <p>Following: {user?.following ?? 0}</p>
+          <p>Public Repos: {user?.public_repos ?? 0}</p>
+          <p>Joined {joinedYear}</p>
+        </div>
+      </a>
+    </div>
   );
 };
 
