9d6e34121a74f6fe60693fad3dce388bdb788076
Ben Smith <ben.smith.dev0@gmail.com>
2023-05-18 18:44:40 -0500

    Add limiting the max number of stored users.

--- src/features/userSearch/userSlice.ts
+++ src/features/userSearch/userSlice.ts
@@ -3,14 +3,41 @@ import {
   getUser,
   PublicGitHubUser,
 } from '../../common/services/publicGitHubApi';
+import { RootState } from '../../app/store';
 
-const sliceDomain = 'users';
+const sliceDomain: string = 'users';
+type UsersSlice = PublicGitHubUser[];
+const maxNumberStoredUsers: number = 10;
 
-export const fetchUser = createAsyncThunk(`${sliceDomain}/fetchUser`, getUser);
+const initialState: UsersSlice = [];
 
-const userSlice = createSlice<PublicGitHubUser[], {}, typeof sliceDomain>({
+const searchedUserFilter = (
+  user: PublicGitHubUser,
+  searchedUsername: string
+): boolean => {
+  return user.login === searchedUsername;
+};
+
+export const fetchUser = createAsyncThunk<
+  PublicGitHubUser,
+  string,
+  { state: RootState }
+>(`${sliceDomain}/fetchUser`, getUser, {
+  condition: (username: string, { getState }) => {
+    const { users } = getState();
+    const cachedUserWithUsername = users.find((user) => {
+      return searchedUserFilter(user, username);
+    });
+
+    // Cancel if user is already in store.
+    return !cachedUserWithUsername;
+  },
+  dispatchConditionRejection: true,
+});
+
+const userSlice = createSlice<UsersSlice, {}, typeof sliceDomain>({
   name: sliceDomain,
-  initialState: [],
+  initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
@@ -18,12 +45,29 @@ const userSlice = createSlice<PublicGitHubUser[], {}, typeof sliceDomain>({
         console.log(`fetching user: ${action.meta.arg}`);
       })
       .addCase(fetchUser.fulfilled, (state, action) => {
-        return [action.payload, ...state];
+        const endIndex: number = Math.min(10, state.length + 1);
+
+        return [action.payload, ...state].slice(0, endIndex);
       })
-      .addCase(fetchUser.rejected, (_, action) => {
+      .addCase(fetchUser.rejected, (state, action) => {
+        if (action.meta.condition) {
+          // Failed condition. Place searched user at the start of list.
+
+          const searchedUsername = action.meta.arg;
+          const searchedUser = state.find((user) =>
+            searchedUserFilter(user, searchedUsername)
+          ) as PublicGitHubUser;
+
+          const otherUsers = state.filter(
+            (user) => !searchedUserFilter(user, searchedUsername)
+          );
+
+          return [searchedUser, ...otherUsers];
+        }
+
         console.log(`Failed to fetch user ${action.meta.arg}`);
 
-        return [];
+        return state;
       });
   },
 });
