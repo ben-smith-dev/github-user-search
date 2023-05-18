cc9ae500a345f4ea69d48afa02ce7d3cdd4f4db8
Ben Smith <ben.smith.dev0@gmail.com>
2023-05-17 21:55:26 -0500

    Add styles for UserCard.

--- src/features/userSearch/UserCard.tsx
+++ src/features/userSearch/UserCard.tsx
@@ -1,26 +1,46 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../../app/store';
+import styles from './userCard.module.css';
 
 const UserCard: React.FC = () => {
   const user = useSelector((state: RootState) => state.user);
 
+  const joinedDate = new Date(user?.created_at ?? '');
+  const joinedYear = joinedDate.getFullYear();
+
   return (
-    <div>
-      <a href={user?.html_url} target="_blank" rel="noreferrer">
-        <h1>{user?.name}</h1>
-        <h2>{user?.login}</h2>
+    <a
+      href={user?.html_url}
+      target="_blank"
+      rel="noreferrer"
+      className={`
+      ${styles.userCard}
+      ${styles.limitedStyle}
+      `}
+    >
+      <div className={styles.cardHeader}>
         <img
           src={user?.avatar_url}
-          height={100}
-          width={100}
           alt="GitHub user's avatar."
+          className={styles.userAvatar}
         ></img>
-        <p>Joined {new Date(user?.created_at ?? '').toDateString()}</p>
+        <div>
+          <h1>{user?.name}</h1>
+          <h2>{user?.login}</h2>
+        </div>
+      </div>
+
+      <div className={styles.cardContent}>
+        <p>{user?.bio ?? 'No bio available.'}</p>
+      </div>
+
+      <div className={styles.cardFooter}>
         <p>Followers: {user?.followers ?? 0}</p>
         <p>Following: {user?.following ?? 0}</p>
         <p>Public Repos: {user?.public_repos ?? 0}</p>
-      </a>
-    </div>
+        <p>Joined {joinedYear}</p>
+      </div>
+    </a>
   );
 };
 
