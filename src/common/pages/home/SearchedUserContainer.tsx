cdb36e88f51bf930f4f26dc670b718c51147045a
Ben Smith <ben.smith.dev0@gmail.com>
2023-05-20 18:23:38 -0500

    Change to display search results.

--- src/common/pages/home/SearchedUserContainer.tsx
+++ src/common/pages/home/SearchedUserContainer.tsx
@@ -1,15 +1,34 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../app/store';
 import UserCard from '../../components/userCard/UserCard';
+import styles from './searchedUserContainer.module.css';
 
 const SearchedUserContainer: React.FC = () => {
-  const user = useSelector((state: RootState) => {
-    return state.users ? state.users[0] : null;
+  const searchResult = useSelector((state: RootState) => {
+    return state.users.searchResult;
   });
 
+  const user = searchResult?.user ?? null;
+
   return (
-    <div>
-      <UserCard user={user} />
+    <div className={styles.searchedUserContainer}>
+      <div className={`${styles.userSearchForm}`}>
+        {searchResult && (
+          <div className={styles.searchResultText}>
+            <p>Showing result for: {searchResult?.searchedUsername}</p>
+
+            <p>{user ? 'user found' : 'no user found'}</p>
+          </div>
+        )}
+      </div>
+
+      <div
+        className={`centerChildrenHorizontal ${
+          user ? styles.scaleVisibilityIn : styles.scaleVisibilityOut
+        }`}
+      >
+        <UserCard user={user} />
+      </div>
     </div>
   );
 };
