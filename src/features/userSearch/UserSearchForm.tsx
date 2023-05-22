f069dc6b40906694b8f1f99ab61f40c2e4652a2a
Ben Smith <ben.smith.dev0@gmail.com>
2023-05-22 18:53:09 -0500

    Add displaying remaining searches available using public GitHub api.

--- src/features/userSearch/UserSearchForm.tsx
+++ src/features/userSearch/UserSearchForm.tsx
@@ -1,12 +1,27 @@
-import { useRef } from 'react';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../../app/store';
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../app/store';
 import { fetchUser } from './usersSlice';
 import SearchForm from '../../common/components/searchForm/SearchForm';
+import styles from './userSearchForm.module.css';
+
+enum InitialAnimationRequiredState {
+  None,
+  InitialRender,
+  NotRequired,
+}
 
 const UserSearchForm: React.FC = () => {
   const userSearch = useRef<HTMLInputElement | null>(null);
+  const [addRateLimitAnimation, setAddRateLimitAnimation] = useState(
+    InitialAnimationRequiredState.None
+  );
+
   const dispatch = useDispatch<AppDispatch>();
+  const rateLimit = useSelector((state: RootState) => state.users.rateLimit);
+  const searchResult = useSelector(
+    (state: RootState) => state.users.searchResult
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
@@ -23,12 +38,66 @@ const UserSearchForm: React.FC = () => {
     }
   };
 
+  useEffect(() => {
+    if (
+      rateLimit &&
+      addRateLimitAnimation === InitialAnimationRequiredState.None
+    ) {
+      setAddRateLimitAnimation(InitialAnimationRequiredState.InitialRender);
+    } else if (
+      addRateLimitAnimation === InitialAnimationRequiredState.InitialRender
+    ) {
+      setAddRateLimitAnimation(InitialAnimationRequiredState.NotRequired);
+    }
+
+    // eslint thinks that we are depending on addRateLimitAnimation to re-render.
+    // eslint-disable-next-line
+  }, [rateLimit]);
+
+  let rateLimitWarningStyle = '';
+  if (rateLimit && rateLimit.remaining <= 0) {
+    rateLimitWarningStyle = `
+      ${
+        !(addRateLimitAnimation === InitialAnimationRequiredState.InitialRender)
+          ? styles.rateLimitCardError
+          : ''
+      }
+      ${styles.limitReached}`;
+  } else if (rateLimit && rateLimit.remaining <= 10) {
+    rateLimitWarningStyle = styles.limitClose;
+  } else if (rateLimit && rateLimit.remaining <= 30) {
+    rateLimitWarningStyle = styles.warning;
+  }
+
   return (
-    <SearchForm
-      onSubmit={onSubmit}
-      searchRef={userSearch}
-      searchPreviewText="Username"
-    />
+    <div className={`${styles.userSearchFormContainer}`}>
+      <div className={`centerChildren ${styles.search}`}>
+        <SearchForm
+          onSubmit={onSubmit}
+          searchRef={userSearch}
+          searchPreviewText="Username"
+        />
+      </div>
+
+      {rateLimit && (
+        <div
+          key={searchResult?.searchedUsername}
+          className={`
+                centerChildren
+                ${styles.rateLimitCard}
+                ${rateLimitWarningStyle}
+                ${
+                  addRateLimitAnimation ===
+                  InitialAnimationRequiredState.InitialRender
+                    ? styles.easeRateLimitIn
+                    : ''
+                }`}
+        >
+          <p>Remaining</p>
+          <p>{rateLimit?.remaining ?? 0}</p>
+        </div>
+      )}
+    </div>
   );
 };
 
