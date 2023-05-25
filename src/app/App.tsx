363b8b195b23667554c308c918b471dee8b5185a
Ben Smith <ben.smith.dev0@gmail.com>
2023-05-25 02:34:24 -0500

    Change to use app router and add 404 fallback page.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../common/pages/home/Home';
+import PageNotFound from '../common/pages/404/PageNotFound';
 import './app.css';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Home />
+    <div className="appContainer"> 
+      <main>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="*" element={<PageNotFound />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
     </div>
   );
 };
