fd5c730cf0a19001b9ecbc2b74463aa73b298088
Ben Smith <ben.smith.dev0@gmail.com>
2023-05-28 00:38:01 -0500

    Remove header.

--- src/common/pages/home/Home.tsx
+++ src/common/pages/home/Home.tsx
@@ -4,7 +4,6 @@ import SearchedUserContainer from './SearchedUserContainer';
 const Home: React.FC = () => {
   return (
     <div className="centerChildrenHorizontal">
-      <h1>GitHub User Search</h1>
       <SearchedUserContainer />
       <StoredUsersContainer />
     </div>
