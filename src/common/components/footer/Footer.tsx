e1506f755c0fcce4e8efdbf120fbfbcdc2a5a287
Ben Smith <ben.smith.dev0@gmail.com>
2023-05-30 20:30:15 -0500

    Add basic footer to navigate to project repo.

---
+++ src/common/components/footer/Footer.tsx
@@ -0,0 +1,25 @@
+import invertocatLogo from '../../assets/github-invertocat-logo.svg';
+import styles from './footer.module.css';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.repositoryLinkContainer}>
+        <a
+          href="https://github.com/ben-smith-dev/github-user-search"
+          rel="noreferrer noopener"
+          target="_blank"
+          title="Opens this web apps GitHub Repository in a new tab."
+        >
+          <img
+            className={styles.invertocatLogo}
+            src={invertocatLogo}
+            alt="GitHub invertocat logo."
+          />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
