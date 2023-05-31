ad0ba2fc9d890c9640f137bd5763563f4f5522c4
Ben Smith <ben.smith.dev0@gmail.com>
2023-05-30 22:59:05 -0500

    Add components index file.

---
+++ src/common/components/index.ts
@@ -0,0 +1,5 @@
+export * from './footer/Footer';
+export * from './header/Header';
+export * from './searchForm/SearchForm';
+export * from './userCard/UserCard';
+export * from './userCardContainer/UserCardContainer';
