d1540295f627d0506fa8c668712d417586b18df5
Ben Smith <ben.smith.dev0@gmail.com>
2023-06-01 18:23:49 -0500

    Refactor usersSlice into index file.

---
+++ src/features/userSearch/usersSlice/index.ts
@@ -0,0 +1,53 @@
+import { SerializedError, createSlice } from '@reduxjs/toolkit';
+import {
+  PublicGitHubUser,
+  RateLimit,
+} from '../../../common/services/publicGitHubApi';
+import {
+  fetchUser,
+  onFetchUserFulfilled,
+  onFetchUserPending,
+  onFetchUserRejected,
+} from './fetchUserThunk';
+
+export interface SliceState {
+  searchedUsers: PublicGitHubUser[];
+  searchResult: {
+    searchedUsername: string;
+    user?: PublicGitHubUser;
+    error?: SerializedError;
+  } | null;
+  rateLimit: RateLimit | null;
+}
+
+const initialState: SliceState = {
+  searchedUsers: [],
+  searchResult: null,
+  rateLimit: null,
+};
+
+const clearUsersReducer = (state: SliceState): SliceState => {
+  return {
+    ...state,
+    searchedUsers: [],
+  };
+};
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    clearUsers: clearUsersReducer,
+  },
+  extraReducers: (builder) => {
+    // Fetch User Action Handlers.
+    builder.addCase(fetchUser.pending, onFetchUserPending);
+    builder.addCase(fetchUser.fulfilled, onFetchUserFulfilled);
+    builder.addCase(fetchUser.rejected, onFetchUserRejected);
+  },
+});
+
+export const { clearUsers } = usersSlice.actions;
+export const users = usersSlice.reducer;
+
+export * from './fetchUserThunk';
