6e64d5288228b29ae9225ad09c965825855eeb3a
Ben Smith <ben.smith.dev0@gmail.com>
2023-06-05 22:10:25 -0500

    Add scale-visibility in/out animation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
     extend: {
       animation: {
         'w-scale-in': 'w-scale-in 0.8s ease-in-out forwards',
-        'h-shake': 'h-shake 300ms ease-in-out forwards'
+        'h-shake': 'h-shake 300ms ease-in-out forwards',
+        'scale-visibility-in': 'scale-visibility-in 100ms ease-in forwards',
+        'scale-visibility-out': 'scale-visibility-out 100ms ease-in forwards'
       },
       keyframes: {
         'w-scale-in': {
@@ -22,7 +24,15 @@ module.exports = {
           '45%': { transform: 'translateX(-0.5rem)' },
           '70%': { transform: 'translateX(0.5rem)' },
           '100%': { transform: 'translateX(0rem)' },
-        }
+        },
+        'scale-visibility-in': {
+          '0%': {transform: 'scale(0)', visibility: 'hidden'},
+          '100%': {transform: 'scale(1)', visibility: 'visible'},
+        },
+        'scale-visibility-out': {
+          '0%': {transform: 'scale(1)', visibility: 'visible'},
+          '100%': {transform: 'scale(0)', visibility: 'hidden'},
+        },
       },
     },
   },
