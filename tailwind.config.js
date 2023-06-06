5237c0e76143b6f18dde430626c662be66a3d0a9
Ben Smith <ben.smith.dev0@gmail.com>
2023-06-05 20:54:15 -0500

    Add horizontal shake animation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,22 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        'w-scale-in': 'w-scale-in 1.2s ease-in-out forwards',
+        'w-scale-in': 'w-scale-in 0.8s ease-in-out forwards',
+        'h-shake': 'h-shake 300ms ease-in-out forwards'
       },
       keyframes: {
         'w-scale-in': {
           '0%' : { width: '0%', 'flex-basis': '0' },
           '100%': { width: '100%', 'flex-basis': 'fit-content' }
+        },
+        'h-shake': {
+          '0%': { transform: 'translateX(0rem)' },
+          '5%': { transform: 'translateX(0.5rem)' },
+          '10%': { transform: 'translateX(-0.5rem)' },
+          '20%': { transform: 'translateX(0.5rem)' },
+          '45%': { transform: 'translateX(-0.5rem)' },
+          '70%': { transform: 'translateX(0.5rem)' },
+          '100%': { transform: 'translateX(0rem)' },
         }
       },
     },
