087a8d8d348d676427341082028a3b7f9551e46c
Ben Smith <ben.smith.dev0@gmail.com>
2023-06-06 18:54:18 -0500

    Refactor to use tailwind.

--- src/common/components/footer/Footer.tsx
+++ src/common/components/footer/Footer.tsx
@@ -1,23 +1,20 @@
 import invertocatLogo from '../../assets/github-invertocat-logo.svg';
-import styles from './footer.module.css';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.repositoryLinkContainer}>
-        <a
-          href="https://github.com/ben-smith-dev/github-user-search"
-          rel="noreferrer noopener"
-          target="_blank"
-          title="Opens this web apps GitHub Repository in a new tab."
-        >
-          <img
-            className={styles.invertocatLogo}
-            src={invertocatLogo}
-            alt="GitHub invertocat logo."
-          />
-        </a>
-      </div>
+    <footer className="w-full h-full flex justify-end py-2 bg-gray-700">
+      <a
+        href="https://github.com/ben-smith-dev/github-user-search"
+        rel="noreferrer noopener"
+        target="_blank"
+        title="Opens this web apps GitHub Repository in a new tab."
+      >
+        <img
+          className="max-w-full max-h-full object-contain"
+          src={invertocatLogo}
+          alt="GitHub invertocat logo."
+        />
+      </a>
     </footer>
   );
 };
