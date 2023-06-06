7e93db0ed06b30413a2acf1960a85c6437c930fc
Ben Smith <ben.smith.dev0@gmail.com>
2023-06-06 18:43:18 -0500

    Refactor to use tailwind.

--- src/common/components/userCardContainer/UserCardContainer.tsx
+++ src/common/components/userCardContainer/UserCardContainer.tsx
@@ -1,6 +1,5 @@
 import { PublicGitHubUser } from '../../services/publicGitHubApi';
 import { UserCard } from '../index';
-import styles from './userCardContainer.module.css';
 
 export interface UserCardContainerProps {
   users: PublicGitHubUser[];
@@ -16,27 +15,27 @@ export const UserCardContainer: React.FC<UserCardContainerProps> = ({
   clearContainer,
 }) => {
   const hasUsers: boolean = 0 < users.length;
-  const buttonClass: string = hasUsers ? `${styles.activeButton}` : ``;
 
   return (
-    <div>
-      <div className={styles.containerHeader}>
+    <div className="@container w-full min-w-[35ch] my-4">
+      <div className="flex justify-between items-end p-2">
         <h3>
           {title} [{users.length}]
         </h3>
 
         <button
           onClick={clearContainer}
-          className={buttonClass}
           disabled={!hasUsers}
+          className="rounded-md p-[0.5rem] transition-all ease-in-out bg-red-600 hover:bg-red-700 active:bg-red-500 
+          disabled:bg-gray-300 disabled:text-gray-700"
         >
           {clearContainerPrompt}
         </button>
       </div>
       <hr />
-      <div className={styles.gridContainer}>
-        {users.map((user, index) => (
-          <div key={index} className="centerChildrenHorizontal">
+      <div className="w-full grid [grid-template-columns:repeat(auto-fit,_minmax(20ch,_70ch))] justify-center gap-4 my-2">
+        {users.map((user) => (
+          <div key={user.id} className="w-full">
             <UserCard user={user} />
           </div>
         ))}
