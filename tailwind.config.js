9c7e80b814ad4756cf339caeee597107bd9fb90d
Ben Smith <ben.smith.dev0@gmail.com>
2023-06-05 20:23:26 -0500

    Add w-scale-in animation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
 ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'w-scale-in': 'w-scale-in 1.2s ease-in-out forwards',
+      },
+      keyframes: {
+        'w-scale-in': {
+          '0%' : { width: '0%', 'flex-basis': '0' },
+          '100%': { width: '100%', 'flex-basis': 'fit-content' }
+        }
+      },
+    },
   },
   plugins: [],
 }
