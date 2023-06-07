ff038f293f66c4192afa412a8db17039978bce45
Ben Smith <ben.smith.dev0@gmail.com>
2023-06-06 19:10:48 -0500

    Refactor to use tailwind.

--- src/common/pages/404/PageNotFound.tsx
+++ src/common/pages/404/PageNotFound.tsx
@@ -1,27 +1,28 @@
 import { useNavigate } from 'react-router';
-import styles from './pageNotFound.module.css';
 
 export const PageNotFound: React.FC = () => {
   const navigate = useNavigate();
 
+  const navigateHome = () => {
+    navigate('/');
+  };
+
   return (
-    <div
-      className={`
-        centerChildren
-        ${styles.container}`}
-    >
-      <div className={styles.header}>
-        <h1>{'OOPS'}</h1>
-        <h2>404: Page not found.</h2>
+    <div className="w-full h-full grid justify-center items-center">
+      <div className="w-fit grid justify-center">
+        <div className="font-bold text-[3em] text-center mb-4">
+          <h1>{'OOPS'}</h1>
+          <h2>404: Page not found.</h2>
+        </div>
+        <button
+          className="text-[2em] px-[2em] py-[1em] mx-auto border-2 border-black rounded-md 
+          hover:bg-gray-400 active:bg-gray-300
+            transition-all ease-in-out"
+          onClick={navigateHome}
+        >
+          Go to Home
+        </button>
       </div>
-      <button
-        className={styles.navigateHome}
-        onClick={() => {
-          navigate('/');
-        }}
-      >
-        Go to Home
-      </button>
     </div>
   );
 };
