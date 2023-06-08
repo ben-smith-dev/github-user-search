389af05a47e5a55eea1d0963df9dc6670a491acf
Ben Smith <ben.smith.dev0@gmail.com>
2023-06-08 01:13:53 -0500

    Add disabling skeleton animation via props.

--- src/common/components/userCard/UserCardSkeleton.tsx
+++ src/common/components/userCard/UserCardSkeleton.tsx
@@ -1,6 +1,14 @@
 import Skeleton from 'react-loading-skeleton';
 
-export const UserCardSkeleton: React.FC = () => {
+interface UserCardSkeletonProps {
+  enableAnimation?: boolean;
+}
+
+export const UserCardSkeleton: React.FC<UserCardSkeletonProps> = ({
+  enableAnimation = true,
+}) => {
+  console.log(enableAnimation);
+
   return (
     <div className="@container w-full h-full p-2 flex justify-center items-center">
       <div
@@ -17,14 +25,18 @@ export const UserCardSkeleton: React.FC = () => {
           @[36rem]:flex-row @[36rem]:gap-4 @[36rem]:items-end`}
         >
           <div className="h-[6rem] w-[6rem] object-contain rounded-md">
-            <Skeleton width="100%" height="100%" />
+            <Skeleton
+              width="100%"
+              height="100%"
+              enableAnimation={enableAnimation}
+            />
           </div>
           <div
             className={`w-[50%] min-w-[12ch] text-center text-[1.5em] h-fit
             @[36rem]:text-left`}
           >
             <h1>
-              <Skeleton count={2} />
+              <Skeleton count={2} enableAnimation={enableAnimation} />
             </h1>
           </div>
         </div>
@@ -32,7 +44,7 @@ export const UserCardSkeleton: React.FC = () => {
         <div className="min-h-[8rem] flex justify-center items-center">
           <div className="h-fit w-full">
             <p>
-              <Skeleton count={4} />
+              <Skeleton count={4} enableAnimation={enableAnimation} />
             </p>
           </div>
         </div>
@@ -42,16 +54,36 @@ export const UserCardSkeleton: React.FC = () => {
           @[36rem]:flex-row @[36rem]:gap-4 @[36rem]:justify-between`}
         >
           <p>
-            <Skeleton width={100} height={20} containerClassName="flex-1" />
+            <Skeleton
+              width={100}
+              height={20}
+              containerClassName="flex-1"
+              enableAnimation={enableAnimation}
+            />
           </p>
           <p>
-            <Skeleton width={100} height={20} containerClassName="flex-1" />
+            <Skeleton
+              width={100}
+              height={20}
+              containerClassName="flex-1"
+              enableAnimation={enableAnimation}
+            />
           </p>
           <p>
-            <Skeleton width={100} height={20} containerClassName="flex-1" />
+            <Skeleton
+              width={100}
+              height={20}
+              containerClassName="flex-1"
+              enableAnimation={enableAnimation}
+            />
           </p>
           <p>
-            <Skeleton width={100} height={20} containerClassName="flex-1" />
+            <Skeleton
+              width={100}
+              height={20}
+              containerClassName="flex-1"
+              enableAnimation={enableAnimation}
+            />
           </p>
         </div>
       </div>
