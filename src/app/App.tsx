e314c679c821a13d983780aad70667e8d54e88e7
Ben Smith <ben.smith.dev0@gmail.com>
2023-06-07 23:57:20 -0500

    Add wrapping app with skeleton theme.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,31 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header, Footer } from '../common/components';
 import { Home, PageNotFound } from '../common/pages';
+import { SkeletonTheme } from 'react-loading-skeleton';
+import { useMediaQuery } from 'usehooks-ts';
 
 const App: React.FC = () => {
+  const prefersDarkScheme = useMediaQuery('(prefers-color-scheme: dark)');
+
   return (
-    <div className="min-w-screen min-h-screen grid grid-rows-[5rem_1fr_5rem] overflow-scroll dark:bg-gray-950">
-      <Header />
-      <main className="w-auto h-full px-1">
-        <BrowserRouter basename="/">
-          <Routes>
-            <Route path="*" element={<PageNotFound />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      </main>
-      <Footer />
-    </div>
+    <SkeletonTheme
+      highlightColor={prefersDarkScheme ? '#9ca3af' : undefined}
+      baseColor={prefersDarkScheme ? '#6b7280' : undefined}
+      duration={2}
+    >
+      <div className="min-w-screen min-h-screen grid grid-rows-[5rem_1fr_5rem] overflow-scroll dark:bg-gray-950">
+        <Header />
+        <main className="w-auto h-full px-1">
+          <BrowserRouter basename="/">
+            <Routes>
+              <Route path="*" element={<PageNotFound />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </BrowserRouter>
+        </main>
+        <Footer />
+      </div>
+    </SkeletonTheme>
   );
 };
 
