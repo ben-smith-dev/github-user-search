f3b849599c8d4d4c2ff40ca0acae437dcafbb8ab
Ben Smith <ben.smith.dev0@gmail.com>
2023-06-08 21:30:08 -0500

    Refactor how theme is set based on color scheme.

--- src/common/components/skeletonWrappers/SkeletonScheme.tsx
+++ src/common/components/skeletonWrappers/SkeletonScheme.tsx
@@ -1,16 +1,45 @@
 import { ReactNode } from 'react';
+import { SkeletonStyleProps, SkeletonTheme } from 'react-loading-skeleton';
 import { useMediaQuery } from 'usehooks-ts';
 
 export interface SkeletonSchemeProps {
-  lightSchemeChildren: ReactNode;
-  darkSchemeChildren: ReactNode;
+  lightModeStyle?: SkeletonStyleProps;
+  darkModeStyle?: SkeletonStyleProps;
+  children: ReactNode;
 }
 
+const defaultDarkModeStyle: SkeletonStyleProps = {
+  baseColor: '#374151', //Tailwind gray 700
+  highlightColor: '#6b7280', // Tailwind gray 500
+};
+
+const defaultLightModeStyle: SkeletonStyleProps = {
+  baseColor: '#9ca3af', // Tailwind gray 500
+  highlightColor: '#d1d5db', // Tailwind gray 300
+};
+
 export const SkeletonScheme: React.FC<SkeletonSchemeProps> = ({
-  lightSchemeChildren,
-  darkSchemeChildren,
+  lightModeStyle,
+  darkModeStyle,
+  children,
 }) => {
   const isDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
-  return <div>{!isDarkMode ? lightSchemeChildren : darkSchemeChildren}</div>;
+  return (
+    <div>
+      {!isDarkMode ? (
+        <SkeletonTheme
+          {...defaultLightModeStyle}
+          {...lightModeStyle}
+          children={children}
+        />
+      ) : (
+        <SkeletonTheme
+          {...defaultDarkModeStyle}
+          {...darkModeStyle}
+          children={children}
+        />
+      )}
+    </div>
+  );
 };
